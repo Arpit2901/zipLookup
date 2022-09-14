b3e4d1355746c656a4beeca4b2b36246e0438701
Arpit verma <arpit.verma@bhavnacorps.com>
2022-09-15 00:33:13 +0530

    adding in file by user

---
+++ task2/src/com/cricket/match/Main.java
@@ -0,0 +1,31 @@
+package com.cricket.match;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		String teamName;
+		int matches;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the name,TeamName,matches");
+		name=sc.next();
+		teamName=sc.next();
+		matches=sc.nextInt();
+		playerDetails obj=new playerDetails(name, teamName, matches);
+		List <String>list=new ArrayList<String>();
+		 list.add(name);  
+		 list.add(teamName);  
+		 list.add(String.valueOf(matches));
+		 for(String ele:list)  
+			 obj.record(ele);
+			  
+		
+		 
+	}
+
+}
