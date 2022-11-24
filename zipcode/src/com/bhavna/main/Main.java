8b1fc7de3a6bbd7322245f0c698474cca6e7f3c6
Arpit Verma <89332105+Arpit2901@users.noreply.github.com>
2022-11-24 11:43:21 +0530

    zipCode first commit

---
+++ zipcode/src/com/bhavna/main/Main.java
@@ -0,0 +1,42 @@
+package com.bhavna.main;
+
+import java.util.Scanner;
+
+import com.bhavna.model.ZipServicev1;
+import com.bhavna.model.ZipServicev2;
+import com.bhavna.model.address;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int choice = 1;
+		Scanner sc = new Scanner(System.in);
+		ZipServicev1 obj = new ZipServicev2();
+		System.out.println("######Zip Lookup Service******");
+		while (choice != 0) {
+			System.out.println("\nEnter !!!!\n1-getting area \n2-setting a new zipcode \n3-print all the zip code");
+			choice = sc.nextInt();
+			if (choice == 1) {
+				System.out.println("Enter the zip code");
+				long code = sc.nextLong();
+				obj.search(code);
+			} else if (choice == 2) {
+				System.out.println("Enter the country,state,area,code");
+				String country = sc.next();
+				String state = sc.next();
+				String area = sc.next();
+				long code = sc.nextLong();
+				obj.addRecord(country, state, area, code);
+			} else if (choice == 3) {
+				System.out.println("*****All the records******");
+				obj.allRecord();
+			} else
+				System.out.println("Wrong choice");
+			System.out.println("Do you want to exit press 0 else any key");
+			choice=sc.nextInt();
+		}
+
+	}
+
+}
---
+++ zipcode/src/com/bhavna/model/ZipServicev1.java
@@ -0,0 +1,15 @@
+package com.bhavna.model;
+
+public abstract class ZipServicev1 {
+	// Abstract class
+	public void search(long code) {
+	}
+
+	public void allRecord() {
+
+	}
+	public void addRecord(String country, String state, String area, long code) {
+		
+	}
+
+}
